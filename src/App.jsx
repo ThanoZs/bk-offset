b10460e2c08cfa486307ee4050670aa68bdee226
ThanoZs <thanozs@users.noreply.github.com>
2026-02-04 14:15:12 +0530

    Machine images fix

--- src/App.jsx
+++ src/App.jsx
@@ -2035,14 +2035,21 @@ function GallerySection({ text, isDark, c, isMobile, isTablet }) {
 
   // FIXED IMAGE LINKS - removed line breaks and backslashes
   const images = [
-    "https://imgs.search.brave.com/0vmwP543aOM5MH0qMkeK-FUMtyKTAednWzujk0DXxIg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5mdGNkbi5uZXQvanBnLzA5LzE3LzAzLzEzLzM2MF9GXzkxNzAzMTM5NV82QjNEMUxKdjM0d29IQnFHSGl6MmZhZmdIN3FHR1pjcS5qcGc",
-    "https://imgs.search.brave.com/4fSMXgmsWcxyR037wKCJJ7Obd2YO5B6jGhf4rdcRkcQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRpYS5pc3RvY2twaG90by5jb20vaWQvNDY2MjIyMzM2L3Bob3RvL21vZGVybi1wcmludGluZy1ob3VzZS5qcGc_cz02MTJ4NjEyJnc9MCZrPTIwJmM9akZDMXVtWFI1RzYwb2NCdE9pWE1VZ09DZkJHR2RFQWx5cmNkUFlsRjFnaz0",
-    "https://imgs.search.brave.com/LRBqEobG2NVqTrIEV2R9gRWI5qmvV1gcVHaBcykcr6k/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5mdGNkbi5uZXQvanBnLzExLzYzLzE0LzAxLzM2MF9GXzExNjMxNDAxNDdfbW94Ylg0aXVGSVhDVEphSkoyQWE4Rk9XQmpwdlhZNUQuanBn",
-    "https://imgs.search.brave.com/e3NaDFicvmSTfGN9LcbRTmQ6q0kUtHZ3lDbjkZYJAAU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0aWMudmVjdGVlenkuY29tL3N5c3RlbS9yZXNvdXJjZXMvdGh1bWJuYWlscy8wMjMvNDQxLzQzMC9zbWFsbC9tb2Rlcm4tcHJpbnRpbmctbWFjaGluZS1wcmVzcy1nZW5lcmF0ZS1haS1waG90by5qcGc",
-    "https://imgs.search.brave.com/XjqWr7jjcNP8AlyvN_DcwTg8anOL8ZXcAyNsczr2H2c/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90aHVtYnMuZHJlYW1zdGltZS5jb20vYi9wcmludGluZy1tYWNoaW5lLTI2OTk4MDg0LmpwZw",
-    "https://imgs.search.brave.com/GOdiMYrLND5c6Oj1kSQ4Q-1qcT7I3PJDq45msauhYak/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0aWMudmVjdGVlenkuY29tL3N5c3RlbS9yZXNvdXJjZXMvdGh1bWJuYWlscy8wNjgvMjk5LzIyOS9zbWFsbC9oaWdoLXNwZWVkLXByaW50aW5nLXByZXNzLWluLWluZHVzdHJpYWwtc2V0dGluZy13aXRoLXN0YWNrcy1vZi1wcmludGVkLXNoZWV0cy1pbi1mYWN0b3J5LWVudmlyb25tZW50LXBob3RvLmpwZWc",
-    "https://imgs.search.brave.com/IVfnN8HiTFa6mYLGXHosj2kRbfcU9L83sbCHLrTZYbw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0aWMudmVjdGVlenkuY29tL3N5c3RlbS9yZXNvdXJjZXMvdGh1bWJuYWlscy8wMjQvNjU0Lzg5My9zbWFsbC9tb2Rlcm4tcHJpbnRpbmctcHJlc3MtcHJvZHVjZXMtbXVsdGktY29sb3JlZC1wcmludG91dHMtYWNjdXJhdGVseS1nZW5lcmF0ZWQtYnktYWktZnJlZS1waG90by5qcGc",
-    "https://imgs.search.brave.com/OR-O7lIPVf3hPrA9x9lcRSRvEI8M-GWD1i6l6uCPgVk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRpYS5pc3RvY2twaG90by5jb20vaWQvNDc1NjkwMTY0L3Bob3RvL3Bvc3QtcHJlc3MtZmluaXNoaW5nLWxpbmUtbWFjaGluZS5qcGc_cz02MTJ4NjEyJnc9MCZrPTIwJmM9Wkw0bEJtUzRELVZPdHRpVUowc1EzQ05QeGh6T1NzQVkydW1tdFVodkZ2RT0",
+    "https://imgs.search.brave.com/d-BxVJB0AVwv7mytORTSGUxHpqMjhHtXeMTPvJqZIPA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/eW91dHViZS5jb20v/dmkvODB6X0hUbVRs/ZDQvaHFkZWZhdWx0/LmpwZw",
+
+    "https://imgs.search.brave.com/HXs3SA9y5tqE3-KGyiE53BmUGqKZaxqNbzgADA568tU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/eW91dHViZS5jb20v/dmkvQy0xaEdEd21a/WEkvaHFkZWZhdWx0/LmpwZw",
+
+    "https://imgs.search.brave.com/8sgKubUmPoPQfFSODo5PPaOi7AUVqGw_TIS14-PxwQE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90aHVt/YnMuZHJlYW1zdGlt/ZS5jb20vYi9wcmVz/cy1wcmludGluZy1w/cmludHNob3Atb2Zm/c2V0LTE2OTMxMzI2/LmpwZw",
+
+    "https://imgs.search.brave.com/prJNhXtQjZFHiNNQsg6lEPcAheXrDNlY1szXvRBQlCo/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTg1/Mjc3MDY5L3Bob3Rv/L29mZnNldC1wcmlu/dGluZy1tYWNoaW5l/LmpwZz9zPTYxMng2/MTImdz0wJms9MjAm/Yz10aHF3cWl2ellU/SEtCdzBxYzgtODBM/UFVvX1AxVHlaMExq/bHQ0RDlhdm0wPQ",
+
+    "https://imgs.search.brave.com/GT4miD1XayG3oiBz9XsPpJ26JFAv4R1pj4j1e_8rwt0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTE5/ODg1OTczL3Bob3Rv/L3BvbHlncmFwaGlj/LXByb2Nlc3MuanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPVp0/NVE3VnpGREJwX1Nj/Z0d5ZVRRTHprRFNO/MUJ1aS1xaV96eW9N/dzl2TEk9",
+
+    "https://imgs.search.brave.com/W3t3GjUQl9eULJkzTE2O694CXodPY8Jn1OZCXSBBGNU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTE5/ODI1NjM0My9waG90/by9wYXBlci10cmlt/bWluZy1vbi1hLWd1/aWxsb3RpbmUtcHJp/bnRpbmctaG91c2Ut/Y3V0dGVyLWZvci1j/dXR0aW5nLXBhcGVy/LmpwZz9zPTYxMng2/MTImdz0wJms9MjAm/Yz0tZlBQRmZpLXBH/Nnp1Q2lyTUxwcUxD/UkdFQV91NE1sNTZu/UXUtTVlRcTRBPQ",
+
+    "https://imgs.search.brave.com/3zVRqhp8m_8bPZQUGAFEZ8pl7YQlHdzqJmMLi0g1SzI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/bWFjaGluZXNlZWtl/ci5jb20vZGF0YS9s/aXN0aW5nL2ltZy92/Z2EvbXMvNTEvNTYv/MjEwNzgyMTItMDgu/anBnP3Y9MTc2OTUw/NDA5NA",
+
+    "https://imgs.search.brave.com/jIEpK8ZUTWenoD4ppVVn1tb3GOdOfbNmJ50jcNh10QQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTc0/OTE0OTM4L3Bob3Rv/L2hlYXZ5LWR1dHkt/cGFwZXItZ3VpbGxv/dGluZS5qcGc_cz02/MTJ4NjEyJnc9MCZr/PTIwJmM9cV82YWEt/djlqQkFYWGdKekNx/bGRrbWkzaFZUSW5m/X1l6MUd2R01EN0E3/WT0",
   ];
 
   return (
